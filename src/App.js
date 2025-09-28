c52d3cbeed32eb1fc54c6d8f0a343cbd4a6dab0b
markogelo14-source <markogelo14@gmail.com>
2025-09-28 11:46:07 +0200

    Only Lorenz

--- src/App.js
+++ src/App.js
@@ -1,77 +1,41 @@
 import "./App.css";
 import { useState } from "react";
-import { Stats } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { Stats } from "@react-three/drei";
 import Particles from "./Particles/Particles";
 import Effects from "./Effects/Effects";
 import Rig from "./Rig/Rig";
 import { useRigMouseEvents } from "./Rig/useRigMouseEvents";
-import Menu from "./Menu/Menu";
 import { AppContainer } from "./AppStyle";
-
-import Error from "./Error/Error";
 import { useParameters } from "./Parameters/useParameters";
 
 export default function App() {
-  const [menuOpen, setMenuOpen] = useState(true);
-  const [func, setFunc] = useState("aizawa");
+  // We keep just enough to render the scene, no UI.
   const [waiting, setWaiting] = useState(false);
-
   const [mouse, onMouseMove, onWheel] = useRigMouseEvents();
-  const [
-    parameters,
-    error,
-    onUpdateParameters,
-    onResetParameters,
-    onError,
-    onClearError,
-  ] = useParameters(func);
+
+  // Lock to the Lorenz attractor (no menu to change it)
+  const [parameters, , , , onError] = useParameters("lorenz");
 
   return (
     <AppContainer
-      menuOpen={menuOpen}
+      menuOpen={false}
       onPointerMove={onMouseMove}
       onWheel={onWheel}
     >
-      <Menu
-        menuOpen={menuOpen}
-        setMenuOpen={setMenuOpen}
-        func={func}
-        setFunc={setFunc}
-        waiting={waiting}
-        setWaiting={setWaiting}
-        parameters={parameters}
-        onUpdateParameters={onUpdateParameters}
-        onResetParameters={onResetParameters}
-      />
-      <Canvas
-        camera={{
-          position: [0, 0, 4],
-          fov: 40,
-          far: 1000,
-        }}
-      >
+      <Canvas camera={{ position: [0, 0, 4], fov: 40, far: 1000 }}>
         <Particles
           parameters={parameters}
           setWaiting={setWaiting}
           onError={onError}
         />
         <Stats className="stats" />
-        <directionalLight
-          intensity={5}
-          position={[-1000, 0, 0]}
-          color="hotpink"
-        />
+        <directionalLight intensity={5} position={[-1000, 0, 0]} color="hotpink" />
         <directionalLight intensity={5} position={[1000, 0, 0]} color="teal" />
-        <directionalLight
-          intensity={5}
-          position={[0, -1000, 0]}
-          color="green"
-        />
+        <directionalLight intensity={5} position={[0, -1000, 0]} color="green" />
         <Effects />
         <Rig mouse={mouse} />
       </Canvas>
-      <Error open={error} onClearError={onClearError} />
     </AppContainer>
   );
 }
